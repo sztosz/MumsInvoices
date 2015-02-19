6eae3933023bb95c8647960847557f00d1bccfa7
Bartosz Nowak <sztosz@gmail.com>
2015-02-19 18:36:14 +0100

    Basic authentication app model

---
+++ authentication/migrations/0001_initial.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Account',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('password', models.CharField(verbose_name='password', max_length=128)),
+                ('last_login', models.DateTimeField(verbose_name='last login', default=django.utils.timezone.now)),
+                ('email', models.EmailField(max_length=75, unique=True)),
+                ('username', models.CharField(max_length=40, unique=True)),
+                ('first_name', models.CharField(blank=True, max_length=40)),
+                ('last_name', models.CharField(blank=True, max_length=40)),
+                ('tagline', models.CharField(blank=True, max_length=140)),
+                ('is_admin', models.BooleanField(default=False)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
+    ]
